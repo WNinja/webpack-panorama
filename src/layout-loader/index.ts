fceb177e68d0431c0f2b25eab8d624233b4b785f
Ninja-EOM-PC <576385849@qq.com>
2023-02-03 18:47:13 +0800

    批量给xml增加样式文件

--- src/layout-loader/index.ts
+++ src/layout-loader/index.ts
@@ -10,6 +10,7 @@ import {
   preserveIncludesAfter,
   preserveIncludesBefore,
   validateIncludes,
+  AddCommonStyles,
 } from './posthtml-plugin-panorama-includes';
 
 export interface PostHTMLLoaderMeta {
@@ -43,6 +44,13 @@ export default async function layoutLoader(
 
     banTextNodes(this),
   ];
+
+  if (options.common_styles) {
+    const files = (options.common_styles as unknown as string[]).map(s => {
+      return path.resolve(this.rootContext, s);
+    });
+    plugins.unshift(AddCommonStyles(files));
+  }
 
   try {
     // 处理导入css写到xml中
